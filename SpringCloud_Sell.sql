ce565ac263bcad859f142ffe5adbc8b9ce6f9840
廖师兄 <lly835@163.com>
2018-03-04 13:33:11 +0800

    新增用户表

--- SpringCloud_Sell.sql
+++ SpringCloud_Sell.sql
@@ -63,3 +63,15 @@ create table `order_detail` (
     primary key (`detail_id`),
     key `idx_order_id` (`order_id`)
 );
+
+-- 用户
+CREATE TABLE `user_info` (
+  `id` varchar(32) NOT NULL,
+  `username` varchar(32) DEFAULT '',
+  `password` varchar(32) DEFAULT '',
+  `openid` varchar(64) DEFAULT '' COMMENT '微信openid',
+  `role` tinyint(1) NOT NULL COMMENT '1买家2卖家',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+);
